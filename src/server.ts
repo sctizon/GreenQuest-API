9b9e3fc40b1441d4f3a4e118002dddcb6732f0d7
StephanJ98 <diego.jeandon@gmail.com>
2024-11-24 00:06:09 +0100

    fix: multiple fix and logger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import expressWinston from "express-winston";
+import winston from "winston";
 
 import eventRoutes from "./routes/eventRoutes";
 import participantRoutes from "./routes/participantRoutes";
@@ -15,6 +17,25 @@ async function main() {
   app.use(cors());
   app.use(express.json());
 
+  // Log the whole request and response body
+  expressWinston.requestWhitelist.push("body");
+  expressWinston.responseWhitelist.push("body");
+
+  // Logger makes sense before the router
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+      ),
+      level: "info",
+      meta: false,
+      msg: "HTTP {{req.method}} {{req.url}}",
+      colorize: false,
+    })
+  );
+
   app.use("/uploads", express.static("uploads")); // Serve static files from the 'uploads' folder
 
   app.use("/api/events", eventRoutes);
@@ -25,6 +46,17 @@ async function main() {
   app.all("*", (req: Request, res: Response) => {
     res.status(404).json({ error: `Route ${req.originalUrl} not found` });
   });
+
+  // Error logger makes sense after the router
+  app.use(
+    expressWinston.errorLogger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+      ),
+    })
+  );
 
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
